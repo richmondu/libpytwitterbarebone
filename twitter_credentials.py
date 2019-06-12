50f66d390391976666e69999bbe7f2980a8f91a4
richmondu <richmond.umagat@yahoo.com>
2019-06-12 15:55:42 +0800

    Update twitter_credentials.py

--- twitter_credentials.py
+++ twitter_credentials.py
@@ -2,9 +2,7 @@
 
 class twitter_credentials:
 
-    CONSUMER_API_KEY    = 'I95ruFgfBP0yLclcLisBaWjxS'
-    CONSUMER_SECRET_KEY = 'VprxEHP3xDtFMAI9l9Y3cAUPSH8Y4FpVzSG95bvDDKFpKvKLBb'
-    ACCESS_TOKEN        = '46786209-oHQILgxFGnO02UKw6UeNqoQVeaZEgBNacD39ZbQdd'
-    ACCESS_SECRET       = 'oub0ZCjUH22O1jYOaQmaZwfw1n79Pziva3FQltK8TuAyB'
-
-
+    CONSUMER_API_KEY    = None
+    CONSUMER_SECRET_KEY = None
+    ACCESS_TOKEN        = None
+    ACCESS_SECRET       = None
